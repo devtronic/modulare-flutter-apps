775ea4e3b9b9f577fa2d1517d46cb70510097039
Julian Finkler <julian@mintware.de>
2023-10-18 14:52:48 +0200

    feat: Show a hint for deleting time tracking entries when deleting a task

--- packages/time_tracking/lib/event_subscriber/task_event_subscriber.dart
+++ packages/time_tracking/lib/event_subscriber/task_event_subscriber.dart
@@ -1,19 +1,28 @@
 import 'package:catalyst_builder/catalyst_builder.dart';
 import 'package:ctwebdev2023_shared/ctwebdev2023_shared.dart';
 import 'package:event_dispatcher_builder/event_dispatcher_builder.dart';
+import 'package:flutter/material.dart';
 
 @Preload()
 @Service(tags: [#eventSubscriber])
 class TaskEventSubscriber {
   final TimeTrackingRepository _repository;
 
-  TaskEventSubscriber(this._repository);
+  final GlobalKey<NavigatorState> _navigatorKey;
+
+  TaskEventSubscriber(this._repository, this._navigatorKey);
 
   @Subscribe()
   void onDeleteTask(DeleteTaskEvent event) {
     var foundEntries = _repository.findEntriesByTaskId(event.task.id);
     if (foundEntries.isNotEmpty) {
-      event.cancelRequested = true;
+      ScaffoldMessenger.of(_navigatorKey.currentContext!).showSnackBar(
+        SnackBar(
+          content: Text(
+            '${foundEntries.length} Zeiterfassungseinträge werden ebenfalls gelöscht.',
+          ),
+        ),
+      );
     }
   }
 
